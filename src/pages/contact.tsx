c5fc132519f39a8fe307d80a7661777a86d9db4a
flowerboi1 <developer.seojoonlee@gmail.com>
2026-05-18 10:13:05 +0900

    misc

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,7 +3,7 @@ function Contact() {
         <div style={{display: "grid", gap: "20px"}}>
             <h1>Contact</h1>
             <h3>Email: seojoonlee@korea.ac.kr</h3>
-            <a href="https://github.com/seojoon1ee"><h3>Github: https://github.com/seojoon1ee</h3></a>
+            <a href="https://github.com/seojoonlee-dev"><h3>Github: https://github.com/seojoonlee-dev</h3></a>
         </div>
     );
 }
